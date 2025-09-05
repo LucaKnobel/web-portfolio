07999dbb1d1a90f9e26668b52876efe31c710b85
Luca Knobel <knobel.luca@gmail.com>
2025-09-05 14:56:26 +0200

    changed config, and fixed fouc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
 export default defineConfig({
+  output: "server",
   i18n: {
     locales: ["de", "en"],
     defaultLocale: "de",
@@ -14,5 +15,4 @@ export default defineConfig({
       iconDir: "src/assets/icons",
     })
   ]
-
 });
