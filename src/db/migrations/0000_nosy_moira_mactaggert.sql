90ec4c7cc4577588d9f3437c0ef2c67c631b91c7
Luca Knobel <knobel.luca@gmail.com>
2026-08-16 16:03:47 +0000

    refactor: remove rate limiting database schema and related migrations

--- src/db/migrations/0000_nosy_moira_mactaggert.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `rate_limits` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`date` text NOT NULL,
-	`count` integer DEFAULT 0 NOT NULL
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `rate_limits_date_unique` ON `rate_limits` (`date`);
