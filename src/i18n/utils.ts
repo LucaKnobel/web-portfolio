dd5011dd9ba01545a924a06da211d29949abed83
Luca Knobel <knobel.luca@gmail.com>
2026-09-06 13:37:56 +0000

    refactor: add getAlternatePath function and integrate into BaseLayout for improved localization

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,9 +1,15 @@
 import { ui, defaultLang } from "./ui";
 
+export type Language = keyof typeof ui;
+
 export function getLangFromUrl(url: URL) {
   const [, lang = ""] = url.pathname.split("/");
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
+}
+
+export function getAlternatePath(pathname: string, language: Language): string {
+  return pathname.replace(/^\/(de|en)(?=\/|$)/, `/${language}`);
 }
 
 export function useTranslations(lang: keyof typeof ui) {
